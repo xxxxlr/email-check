2836f5ec5035fd42ca70fff18c633058c39f3077
xxxxlr <xxxxlr@gmail.com>
2018-02-08 14:33:41 +0800

    Update index.js
    Added logic to check against all exchanges sequentially.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var promisify = require('js-promisify');
 
 // Helper to validate email based on regex
 const EMAIL_REGEX = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+const LOG_FLAG = false
 
 function validateEmail (email) {
   if (typeof email === 'string' && email.length > 5 && email.length < 61 && EMAIL_REGEX.test(email)) {
@@ -41,10 +42,16 @@ module.exports = function (email, opts) {
             lowestPriorityIndex = i;
           }
         }
-        return addresses[lowestPriorityIndex].exchange;
+        // Get all addresses's exchanges
+        var exchanges = []
+        for (var i = 0; i < addresses.length; i++) {
+          exchanges.push(addresses[i].exchange)
+        }
+        return exchanges
+        // return addresses[lowestPriorityIndex].exchange;
       }
     })
-    .then(function (address) {
+    .then(function (addresses) {
       opts = opts || {};
       var options = {
         from: validateEmail(opts.from) || email,
@@ -57,35 +64,60 @@ module.exports = function (email, opts) {
         'mail from:<' + options.from + '>\n',
         'rcpt to:<' + email + '>\n'
       ];
-      return new Promise(function (resolve, reject) {
-        var socket = net.createConnection(25, address);
-        socket.setTimeout(options.timeout, function () {
-          socket.destroy();
-          resolve(false);
-        });
-        socket.on('data', function (data) {
-          if (data.toString()[0] !== '2') {
+      var ps = []
+      for(var i = 0; i < addresses.length; i++){
+        var address = addresses[i]
+        var pFunc = ((address) => new Promise(function (resolve, reject) {
+          log('trying:' + address)
+          var socket = net.createConnection(25, address);
+          socket.setTimeout(options.timeout, function () {
+            log('f:' + address)
             socket.destroy();
-            reject(new Error('refuse'));
-          }
-          if (step < 3) {
-            socket.write(COMM[step], function () {
-              step++;
-            });
-          } else {
+            resolve(false);
+          });
+          socket.on('data', function (data) {
+            if (data.toString()[0] !== '2') {
+              socket.destroy();
+              reject(new Error('refuse'));
+            }
+            if (step < 3) {
+              socket.write(COMM[step], function () {
+                step++;
+              });
+            } else {
+              socket.destroy();
+              resolve(true);
+            }
+          });
+          socket.on('error', function (err) {
             socket.destroy();
-            resolve(true);
+            if (err.code === 'ECONNRESET') {
+              reject(new Error('refuse'));
+            } else {
+              reject(err);
+            }
+          })
+        })).bind(this, address)
+
+        ps.push( pFunc )
+      }
+      log('After structured all promises')
+      return ps.reduce(function(prev, current, index){
+        return prev.then(function(res){
+          if(res === true){
+            log(index + '->skip current')
+            return Promise.resolve(true)
           }
-        });
-        socket.on('error', function (err) {
-          socket.destroy();
-          if (err.code === 'ECONNRESET') {
-            reject(new Error('refuse'));
-          } else {
-            reject(err);
-          }
+          log(index + '->checking current')
+          return current()
+        }).catch(function(err){
+          log('prev got rejected in catch')
+          error(err)
+          // return Promise.reject(err)
+          return Promise.resolve(false)
         })
-      });
+      }, Promise.resolve(false));
+     
     })
     .catch(function (err) {
       if (err.message === 'wrong') {
@@ -95,3 +127,9 @@ module.exports = function (email, opts) {
       }
     })
 };
+
+function log(message){
+  if(LOG_FLAG){
+    console.log(message)
+  }
+}
