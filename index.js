f347a14d5d654496da53760f7a8ee41bba0e3543
xxxxlr <xxxxlr@gmail.com>
2018-02-08 16:41:39 +0800

    Update index.js
    Changed return format

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var promisify = require('js-promisify');
 
 // Helper to validate email based on regex
 const EMAIL_REGEX = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-const LOG_FLAG = false
+const LOG_FLAG = true
 
 function validateEmail (email) {
   if (typeof email === 'string' && email.length > 5 && email.length < 61 && EMAIL_REGEX.test(email)) {
@@ -18,114 +18,119 @@ function validateEmail (email) {
 
 // Full email check
 module.exports = function (email, opts) {
+  log('<<<<start:' + email)
   return new Promise(function (resolve, reject) {
     email = validateEmail(email);
     email ? resolve(email.split('@')[1]) : reject();
   })
-    .then(function (domain) {
-      return promisify(dns.resolveMx, [domain]);
-    })
-    .catch(function (err) {
-      throw(new Error('wrong'));
-    })
-    .then(function (addresses) {
-      if (addresses.length === 1) {
-        return addresses[0].exchange;
-      } else {
-        // Find the lowest priority mail server
-        var lowestPriorityIndex = 0;
-        var lowestPriority = addresses[0].priority;
-        for (var i = 1; i < addresses.length; i++) {
-          var currentPriority = addresses[i].priority;
-          if (currentPriority < lowestPriority) {
-            lowestPriority = currentPriority;
-            lowestPriorityIndex = i;
-          }
-        }
-        // Get all addresses's exchanges
-        var exchanges = []
-        for (var i = 0; i < addresses.length; i++) {
-          exchanges.push(addresses[i].exchange)
-        }
-        return exchanges
-        // return addresses[lowestPriorityIndex].exchange;
+  .then(function (domain) {
+    return promisify(dns.resolveMx, [domain]);
+  })
+  .catch(function (err) {
+    throw(new Error('wrong'));
+  })
+  .then(function (addresses) {
+    if (addresses.length === 1) {
+      return addresses[0].exchange;
+    } else {
+      // Find the lowest priority mail server
+      // var lowestPriorityIndex = 0;
+      // var lowestPriority = addresses[0].priority;
+      // for (var i = 1; i < addresses.length; i++) {
+      //   var currentPriority = addresses[i].priority;
+      //   if (currentPriority < lowestPriority) {
+      //     lowestPriority = currentPriority;
+      //     lowestPriorityIndex = i;
+      //   }
+      // }
+      // return addresses[lowestPriorityIndex].exchange;
+      // Get all addresses's exchanges
+      var exchanges = []
+      for (var i = 0; i < addresses.length; i++) {
+        exchanges.push(addresses[i].exchange)
       }
-    })
-    .then(function (addresses) {
-      opts = opts || {};
-      var options = {
-        from: validateEmail(opts.from) || email,
-        timeout: opts.timeout || 5000
-      };
-      options.host = opts.host || options.from.split('@')[1];
-      const COMM = [
-        'helo ' + options.host + '\n',
-        'mail from:<' + options.from + '>\n',
-        'rcpt to:<' + email + '>\n'
-      ];
-      var ps = []
-      for(var i = 0; i < addresses.length; i++){
-        var address = addresses[i]
-        var pFunc = ((address) => new Promise(function (resolve, reject) {
-          var step = 0;
-          log('trying:' + address)
-          var socket = net.createConnection(25, address);
-          socket.setTimeout(options.timeout, function () {
-            log('f:' + address)
+      return exchanges
+    }
+  })
+  .then(function (addresses) {
+    opts = opts || {};
+    var options = {
+      from: validateEmail(opts.from) || email,
+      timeout: opts.timeout || 5000
+    };
+    options.host = opts.host || options.from.split('@')[1];
+    // NOTE: \r\n is more compitable than \n
+    const end = '\r\n'
+    const COMM = [
+      'HELO ' + options.host + end,
+      'MAIL FROM: <' + options.from + '>' + end,
+      'RCPT TO: <' + email + '>' + end
+    ];
+    var ps = []
+    for(var i = 0; i < addresses.length; i++){
+      var address = addresses[i]
+      var pFunc = ((address) => new Promise(function (resolve, reject) {
+        var step = 0;
+        
+        log('trying:' + address)
+        var socket = net.createConnection(25, address);
+        socket.setTimeout(options.timeout, function () {
+          log('timeout after ' + options.timeout + ' for ' + address)
+          socket.destroy();
+          resolve(false);
+        });
+        socket.on('data', function (data) {
+          log(data.toString())
+          if (data.toString()[0] !== '2') {
             socket.destroy();
-            resolve(false);
-          });
-          socket.on('data', function (data) {
-            if (data.toString()[0] !== '2') {
-              socket.destroy();
-              reject(new Error('refuse'));
-            }
-            if (step < 3) {
-              socket.write(COMM[step], function () {
-                step++;
-              });
-            } else {
-              socket.destroy();
-              resolve(true);
-            }
-          });
-          socket.on('error', function (err) {
-            socket.destroy();
-            if (err.code === 'ECONNRESET') {
-              reject(new Error('refuse'));
-            } else {
-              reject(err);
-            }
-          })
-        })).bind(this, address)
-
-        ps.push( pFunc )
-      }
-      log('After structured all promises')
-      return ps.reduce(function(prev, current, index){
-        return prev.then(function(res){
-          if(res === true){
-            log(index + '->skip current')
-            return Promise.resolve(true)
+            reject(new Error('Not 2xx return in last message:' + data.toString()));
           }
-          log(index + '->checking current')
-          return current()
-        }).catch(function(err){
-          log('prev got rejected in catch')
-          error(err)
-          // return Promise.reject(err)
-          return Promise.resolve(false)
+          if (step < 3) {
+            socket.write(COMM[step], function () {
+              step++;
+            });
+          } else {
+            socket.destroy();
+            resolve(true);
+          }
+        });
+        socket.on('error', function (err) {
+          log('socket error callback:')
+          log(JSON.stringify(err))
+          socket.destroy();
+          if (err.code === 'ECONNRESET') {
+            reject(new Error('refuse: ' + err.code));
+          } else {
+            reject(err);
+          }
         })
-      }, Promise.resolve(false));
-     
-    })
-    .catch(function (err) {
-      if (err.message === 'wrong') {
-        return false;
-      } else {
-        throw err;
-      }
-    })
+      })).bind(this, address)
+
+      ps.push( pFunc )
+    }
+    log('After structured all promises')
+    // Check against all exchange servers until find a match or return error when all servers say it invalid 
+    return ps.reduce(function(prev, current, index){
+      return prev.then(function(res){
+        if(res.status === true){
+          log(index + '->skip current')
+          return Promise.resolve({status: true, error: null})
+        }
+        log(index + '->checking current')
+        return current()
+      }).catch(function(err){
+        return Promise.resolve({status: false, error: err})
+      })
+    }, Promise.resolve({status: false, error: null}));
+    
+  })
+  .catch(function (err) {
+    if (err.message === 'wrong') {
+      return false;
+    } else {
+      throw err;
+    }
+  })
 };
 
 function log(message){
